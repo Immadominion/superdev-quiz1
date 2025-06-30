b8dbf275399979b0454be23b922992a56415a7c1
Nwakanma Dominion <118551746+Immadominion@users.noreply.github.com>
2025-06-30 18:35:46 +0100

    fix port issue

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse::<u16>()
+        .unwrap_or(8080);
+
     let cors_config = warp::cors()
         .allow_any_origin()
         .allow_headers(vec!["content-type"])
@@ -14,6 +19,6 @@ async fn main() {
 
     let all_routes = setup_routes_superdev().with(cors_config);
 
-    println!("Superdev Quiz Server running on http://0.0.0.0:3030");
-    warp::serve(all_routes).run(([0, 0, 0, 0], 3030)).await;
+    println!("Superdev Quiz Server running on http://0.0.0.0:{}", port);
+    warp::serve(all_routes).run(([0, 0, 0, 0], port)).await;
 }
